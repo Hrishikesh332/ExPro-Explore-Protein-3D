2427102ac8b34f02eec350cab4886c255fecb476
Hrishikesh <hrishikesh3321@gmail.com>
2023-01-20 02:16:42 +0530

    Updated ChemPlay Section

--- app.py
+++ app.py
@@ -165,28 +165,30 @@ if (selected=="Explore"):
         st.balloons()
 
 if (selected=="ChemPlay"):
+    st.markdown("<h1 style='text-align: center; '>ChemPlay - Let's Play Quiz 🎮</h1>", unsafe_allow_html=True)
+    st.markdown("---")
     if 'num' not in st.session_state:
         st.session_state.num = 0
 
 
-    choices1 = ['no answer', 'manila', 'tokyo', 'bangkok']
-    choices2 = ['no answer', 'thailand', 'japan', 'philippines']
+    choices1 = ['Hydrogen', 'Carbon', 'Oxygen', 'Amino']
+    choices2 = ['Shape', 'Taste', 'Reactivity','None of these']
+    choices3 = ['Defend Body from harmful microorganisms', 'Hair', 'Muscle','None of these']
+    choices4 = ['True','False']
 
-    qs1 = [('What is the capital of Japan', choices1),
-        ('What is the capital of Philippines', choices1),
-        ('What is the capital of Thailand', choices1)]
-    qs2 = [('What country has the highest life expectancy?', choices2),
-        ('What country has the highest population?', choices2),
-        ('What country has the highest oil deposits?', choices2)]
+    qs1 = [('What is the central atom in an Amino Acid?', choices1),
+        ('What determines a Proteins function?', choices2)]
+    qs2 = [('hich of these is NOT a function of Protein?', choices3),
+        ('Athletes need much more protein than other people?', choices4)]
 
 
-    def main():
-        for _, _ in zip(qs1, qs2): 
+    def quiz():
+        for _, _ in zip(q1, q2): 
             placeholder = st.empty()
             num = st.session_state.num
             with placeholder.form(key=str(num)):
-                st.radio(qs1[num][0], key=num+1, options=qs1[num][1])
-                st.radio(qs2[num][0], key=num+1, options=qs2[num][1])          
+                st.radio(q1[num][0], key=num+1, options=q1[num][1])
+                st.radio(q2[num][0], key=num+1, options=q2[num][1])          
                         
                 if st.form_submit_button():
                     st.session_state.num += 1
@@ -197,7 +199,7 @@ if (selected=="ChemPlay"):
                     st.stop()
 
 
-    main()
+    quiz()
 
 if (selected=="Suggestion"):
     st.title("Suggestions:")
